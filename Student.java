bdfb97270d7fc4b7b38895578c50c1c1fe795b69
Pendyala Bhagyalaxmi <151535131+bhagyalaxmi2002@users.noreply.github.com>
2024-07-18 20:49:46 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,28 @@
+package ctrlstmt;
+
+public class Student {
+	private String Stdname  ; 
+	private String Collegename;
+	 private int Stdid;
+	 
+	public Student(String Stdname, String Collegename, int Stdid) {
+		this.Stdname ="Bhagyalaxmi";
+		this.Collegename ="Avanthi";
+		this.Stdid = 49;
+	}
+
+	public static void main(String[] args) {
+		//Student student = new Student("Bhagyalaxmi", "avanthi", 49);
+	
+		System.out.println("Successfull");	
+		
+		
+	}
+	 
+	
+	 
+	 
+	
+	}
+
+
