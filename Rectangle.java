37d9ed5baff40acfb9cee5ec11385aa6f944ba0e
Pendyala Bhagyalaxmi <151535131+bhagyalaxmi2002@users.noreply.github.com>
2024-07-18 22:15:20 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,18 @@
+package ctrlstmt;
+
+public class Rectangle {
+	double width;
+	double height;
+	Color boxColor;
+	
+	enum Color{
+		RED, GREEN, BLUE
+	}
+	
+                 
+	public static void main(String[] args) {
+		
+System.out.println(Color.BLUE);
+	}
+
+}
