890545b12e2df9968846e6123510a4305a93fd54
Pendyala Bhagyalaxmi <151535131+bhagyalaxmi2002@users.noreply.github.com>
2024-07-18 21:42:29 +0530

    Add files via upload

---
+++ Employe.java
@@ -0,0 +1,16 @@
+package ctrlstmt;
+
+public class Employe {
+	protected int Id;
+	protected int age;
+	protected String name;
+	protected boolean isPermanent;
+
+	public static void main(String[] args) {
+		Employe Emp = new Employe();
+		Emp.age = (int)35.5;
+		System.out.println("Sucessfully Started");
+
+	}
+
+}
