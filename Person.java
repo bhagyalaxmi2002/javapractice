22e1c0908a5307620169983352d95ed4c7ed8127
Pendyala Bhagyalaxmi <151535131+bhagyalaxmi2002@users.noreply.github.com>
2024-07-18 21:58:52 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,13 @@
+package com.anudip.learning;
+
+public class Person {
+      String name = "Geeta";
+      int age = 25;
+      int salary = 25000;
+	public static void main(String[] args) {
+		
+		System.out.println("Test Sucessfully");
+
+	}
+
+}
