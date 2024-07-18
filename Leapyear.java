587599631f03f415b362502a911005d2c8f38976
Pendyala Bhagyalaxmi <151535131+bhagyalaxmi2002@users.noreply.github.com>
2024-07-18 19:33:52 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,16 @@
+package ctrlstmt;
+
+public class Leapyear {
+
+	public static void main(String[] args) {
+		int year = 2024;
+		if(year%4==0) {
+			System.out.println("Leap year");
+		}
+		else {
+			System.out.println("Not a leap year");
+		}
+
+	}
+
+}
---
+++ SimpleInterest.java
@@ -0,0 +1,19 @@
+package ctrlstmt;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+		 double principal = 15000;  
+		 double rate = 5;          
+	        int time = 2;            
+
+	        
+	        double simpleInterest = (principal * rate * time) / 100;
+
+	        
+	        System.out.println("The Simple Interest is: " + simpleInterest);
+
+
+	}
+
+}
